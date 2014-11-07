da32bff1003cfa797fe3945d98b47b0d3941baa2
Jinhyun Kim <jinkim608@gmail.com>
2014-11-07 13:54:10 -0500

    Updating gitignore after fixing import issue updating package name

--- reachiodemo/app/src/main/java/io/reach/reachiodemo/Bus/BusProvider.java
+++ reachiodemo/app/src/main/java/io/reach/reachiodemo/Bus/BusProvider.java
@@ -26,11 +26,11 @@ import com.squareup.otto.Bus;
 public final class BusProvider {
     private static final Bus BUS = new Bus();
 
-    public static Bus getInstance() {
-        return BUS;
-    }
-
     private BusProvider() {
         // No instances.
+    }
+
+    public static Bus getInstance() {
+        return BUS;
     }
 }
